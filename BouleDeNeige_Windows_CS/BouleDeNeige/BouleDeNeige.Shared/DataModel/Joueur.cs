fb00db37b5e87420687406eba287ddad4b6dd227
Yan Grenier <ygrenier@ygrenier.com>
2015-12-08 20:21:10 +0100

    Création de la classe "Joueur" pour les clients. Test de création et de recherche des joueurs sur l'API local.

---
+++ BouleDeNeige_Windows_CS/BouleDeNeige/BouleDeNeige.Shared/DataModel/Joueur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BouleDeNeige
+{
+    public class Joueur
+    {
+        public String Id { get; set; }
+        public String Nom { get; set; }
+        public int BoulesRestantes { get; set; }
+        public int BoulesRecues { get; set; }
+        public int BoulesLancees { get; set; }
+        public int Points { get; set; }
+    }
+}
--- BouleDeNeige_Windows_CS/BouleDeNeige/BouleDeNeige.Windows/MainPage.xaml.cs
+++ BouleDeNeige_Windows_CS/BouleDeNeige/BouleDeNeige.Windows/MainPage.xaml.cs
@@ -1,8 +1,37 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
 
 namespace BouleDeNeige
 {
     public sealed partial class MainPage : Page
     {
+        private async void btnTest_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            // Création d'une référence sur la table des joueurs
+            IMobileServiceTable<Joueur> jTable = App.MobileService.GetTable<Joueur>();
+            // Requête pour rechercher les joueurs
+            var j1 = (await jTable.CreateQuery().Where(j => j.Nom == "Joueur 1").ToListAsync()).FirstOrDefault();
+            var j2 = (await jTable.CreateQuery().Where(j => j.Nom == "Joueur 2").ToListAsync()).FirstOrDefault();
+            // Pour chaque joueur manquant on les créés
+            if (j1 == null)
+            {
+                j1 = new Joueur()
+                {
+                    Nom = "Joueur 1",
+                };
+                await jTable.InsertAsync(j1);
+            }
+            if(j2== null)
+            {
+                j2 = new Joueur()
+                {
+                    Nom = "Joueur 2"
+                };
+                await jTable.InsertAsync(j2);
+            }
+            System.Diagnostics.Debug.WriteLine(j1.Id);
+            System.Diagnostics.Debug.WriteLine(j2.Id);
+        }
     }
 }
