13ccc1d45b80b3547693fef2ffd7468f34988076
Yan Grenier <ygrenier@ygrenier.com>
2015-12-09 20:05:24 +0100

    Mise en place du client de service.

---
+++ BouleDeNeige_Windows_CS/BouleDeNeige/BouleDeNeige.Shared/ServiceClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BouleDeNeige
+{
+    /// <summary>
+    /// Classe de gestion du service
+    /// </summary>
+    public class ServiceClient
+    {
+#if DEBUG
+        const String appUrl = "http://localhost:59487/";
+#else
+        const String appUrl = "https://bouledeneige.azurewebsites.net";
+#endif
+
+        /// <summary>
+        /// Création d'un nouveau client de service
+        /// </summary>
+        public ServiceClient()
+        {
+            MobileService = new MobileServiceClient(appUrl);
+        }
+
+        /// <summary>
+        /// Service mobile
+        /// </summary>
+        public MobileServiceClient MobileService { get; private set; }
+    }
+}
